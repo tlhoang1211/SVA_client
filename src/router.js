53b36a2c718c8c4c601d9254fb23818daf81a247
Hoàng Trần <46571690+tlhoang1211@users.noreply.github.com>
2021-06-21 21:52:07 +0700

    client

---
+++ src/router.js
@@ -0,0 +1,11 @@
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login', '/register', '/home'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
+});
